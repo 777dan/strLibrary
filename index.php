ba899e50e2b1a2a1f3c994c8c7ac5dd904ff8a9c
777dan <danya.nikitin7@gmail.com>
2022-10-24 13:43:42 +0300

    v 1.0

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$str = "There are many cats in our house.";
+$arr = explode(" ", $str);
+function reverseSorting()
+{
+    krsort($GLOBALS['arr']);
+}
+function deleteWords()
+{
+    for ($i = 0; $i < count($GLOBALS['arr']); $i++) {
+        if ($i % 2 != 0) {
+            $GLOBALS["arr"][$i] = "";
+            $GLOBALS["str1"] = implode(" ", $GLOBALS["arr"]);
+        }
+    }
+}
+function firstLeter() {
+    for ($i = 0; $i < count($GLOBALS['arr']); $i++) {
+        echo $GLOBALS['arr'][$i][0];
+    }
+}
+function getFunction() {
+    call_user_func('firstLeter');
+}
+getFunction();
