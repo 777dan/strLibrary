2cbbc062e62517b7cb684b3afdeb088c206004cd
777dan <danya.nikitin7@gmail.com>
2022-10-26 09:19:32 +0300

    v 2.0

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
 $str = "There are many cats in our house.";
 $arr = explode(" ", $str);
-function reverseSorting()
-{
-    krsort($GLOBALS['arr']);
-}
-function deleteWords()
-{
-    for ($i = 0; $i < count($GLOBALS['arr']); $i++) {
-        if ($i % 2 != 0) {
-            $GLOBALS["arr"][$i] = "";
-            $GLOBALS["str1"] = implode(" ", $GLOBALS["arr"]);
-        }
-    }
-}
-function firstLeter() {
-    for ($i = 0; $i < count($GLOBALS['arr']); $i++) {
-        echo $GLOBALS['arr'][$i][0];
-    }
-}
+require_once "library.inc";
 function getFunction() {
     call_user_func('firstLeter');
 }
 getFunction();
+// function reverseSorting()
+// {
+//     krsort($GLOBALS['arr']);
+// }
+// function deleteWords()
+// {
+//     for ($i = 0; $i < count($GLOBALS['arr']); $i++) {
+//         if ($i % 2 != 0) {
+//             $GLOBALS["arr"][$i] = "";
+//             $GLOBALS["str1"] = implode(" ", $GLOBALS["arr"]);
+//         }
+//     }
+// }
+// function firstLeter() {
+//     for ($i = 0; $i < count($GLOBALS['arr']); $i++) {
+//         echo $GLOBALS['arr'][$i][0];
+//     }
+// }
